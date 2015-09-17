e70bd1c382e5033ff06210f2d1fb694442c67c09
hazrmard <ibragim78786@gmail.com>
2015-09-17 13:57:40 -0500

    Testing local vs hosted CORS errors

---
+++ script.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function() {
+	console.log("ready!");
+	 $.ajax({
+        type: "GET",
+        url: "/tips.txt",
+        dataType: "text",
+        success: function(data) {processData(data);}
+     });
+	
+});
+
+function processData(allText) {
+	var allTextLines = allText.split(/\r\n|\n/);
+	console.log(allTextLines);
+}
