0c42959216379de12a93998e23f558025bb1e25d
hazrmard <ibragim78786@gmail.com>
2015-09-18 10:42:42 -0500

    Basic site layout

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 ﻿$(document).ready(function() {
 	console.log("ready!");
-	 $.ajax({
-        type: "GET",
-        url: "/tips.txt",
-        dataType: "text",
-        success: function(data) {processData(data);}
-     });
+	getTextFile("http://hazrmard.github.io/RelaxVandy/tips.txt");
 	
 });
 
+//get txt file
+function getTextFile(path) {
+	 // loading local text file containing data
+	 $.ajax({
+        type: "GET",
+        url: path,
+        dataType: "text",
+        success: function(data) {processData(data);}
+     });
+};
+
+//split raw data into array of lines
 function processData(allText) {
 	var allTextLines = allText.split(/\r\n|\n/);
+	allTextLines = filterList(allTextLines, "");
 	console.log(allTextLines);
-}
+};
+
+//filter out undesired strings e.g. empty lines
+function filterList(data, what) {
+	return data.filter(function(element) {
+		return element !==what;
+	});
+};
